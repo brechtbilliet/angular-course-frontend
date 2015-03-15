4623d13b8c49c3f93798f2cc27079ee4d0c6889f
Brecht Billiet <brechtbilliet@Brechts-MBP.lan>
2015-03-15 10:07:39 +0100

    initial

--- app/module/customer/controller/addController.js
+++ app/module/customer/controller/addController.js
@@ -1,37 +1,49 @@
-(function(){
+(function() {
 	'use strict';
-	function Constructor($location, customerService, CONFIG, toastr){
+
+	function Constructor($location, customerService, CONFIG, toastr, preventLeaveService) {
 		var vm = this;
 		vm.save = save;
 		vm.cancel = cancel;
 		vm.workingCopyChanged = workingCopyChanged;
 
-		function workingCopyChanged(){
+		function workingCopyChanged() {
 			vm.dirty = !angular.equals(vm.workingCopy, vm.originalCopy);
+			if (vm.dirty) {
+				preventLeaveService.prevent();
+			}
+			else{
+				preventLeaveService.allow();
+			}
 		}
 
-		function save(){
-			function onSuccess(response){
+		function save() {
+			function onSuccess(response) {
+				preventLeaveService.allow();
 				$location.path('customers');
 				toastr.success(CONFIG.toasts.successfullySavedData);
 			}
-			function onFail(response){
+
+			function onFail(response) {
+			preventLeaveService.allow();
 				toastr.error(CONFIG.toasts.failedToSaveData);
 
 			}
 			customerService.add(vm.workingCopy).then(onSuccess, onFail);
 		}
-		function cancel(){
+
+		function cancel() {
 			$location.path('customers');
+			preventLeaveService.allow();
 		}
-	
-		function initVm(){
+
+		function initVm() {
 			vm.dirty = false;
 			vm.workingCopy = customerService.createEmpty();
 			vm.originalCopy = angular.copy(vm.workingCopy);
 		}
 		initVm();
 	}
-	Constructor.$inject = ['$location', 'customerService', 'CONFIG', 'toastr'];
+	Constructor.$inject = ['$location', 'customerService', 'CONFIG', 'toastr', 'preventLeaveService'];
 	angular.module('app.customer').controller('customer_addController', Constructor);
 }());
---
+++ app/service/preventLeaveService.js
@@ -0,0 +1,38 @@
+(function() {
+	'use strict';
+
+	function Service($rootScope, toastr) {
+		var preventLeave = false;
+		var reason = null;
+		return {
+			init: init,
+			allow: allow,
+			prevent: prevent,
+			canLeave: canLeave
+		};
+		function init(){
+				debugger;
+			function onLocationChangeStart(e){
+				if(prevent){
+					e.preventDefault();
+					toastr.warning(reason);
+				}
+			}
+			$rootScope.$on('$locationChangeStart', onLocationChangeStart);
+		}
+
+		function allow(){
+			reason = null;
+			preventLeave = false;
+		}
+		function prevent(passedReason){
+			preventLeave = true;
+			reason = passedReason;
+		}
+		function canLeave(){
+			return !preventLeave;
+		}
+	}
+	Service.$inject = ['$rootScope', 'toastr'];
+	angular.module('app').factory('preventLeaveService', Service);
+}());
