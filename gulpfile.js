afc975b7046f1fc419b1c036fffc9b91f8888c87
Brecht Billiet <brecht.billiet@persgroep.be>
2015-03-19 13:13:18 +0100

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,6 @@
 			.pipe(concat('scripts.js'))
 			.pipe(sourcemaps.write())
 			.pipe(gulp.dest('dev'))
-			.pipe(connect.reload());
 	});
 
 	gulp.task('compile-css', function() {
@@ -54,7 +53,6 @@
 			.pipe(concat('screen.css'))
 			.pipe(sourcemaps.write())
 			.pipe(gulp.dest('dev'))
-			.pipe(connect.reload());
 	});
 	gulp.task('uncache-index', function() {
 		return gulp.src(['index.html', 'config.json'])
@@ -82,7 +80,7 @@
 		return deferred.promise;
 	});
 
-	
+
 	/* interaction */
 	gulp.task('start-server', function() {
 		connect.server({
@@ -92,26 +90,34 @@
 	});
 
 	gulp.task('watch-changes', function() {
-		gulp.watch(['app/*.js', 'app/**/*.js'], ['compile-javascript', 'uncache-index']);
-		gulp.watch('app/**/*.html', ['compile-javascript', 'uncache-index']);
-		gulp.watch('style/screen.scss', ['compile-css']);
-		gulp.watch('index.html', ['uncache-index']);
+		gulp.watch(['app/*.js', 'app/**/*.js'], function() {
+			runSequence('compile-javascript', 'uncache-index');
+		});
+		gulp.watch('app/**/*.html', function() {
+			runSequence('compile-javascript', 'uncache-index');
+		});
+		gulp.watch('style/screen.css', function() {
+			runSequence('compile-css', 'uncache-index');
+		});
+		gulp.watch('index.html', function() {
+			runSequence('uncache-index');
+		});
 	});
-	gulp.task('open-browser', function() {
-		var options = {
-			url: 'http://localhost:8080'
-		};
-		return gulp.src('./index.html')
-			.pipe(gOpen('', options));
-	});
+gulp.task('open-browser', function() {
+	var options = {
+		url: 'http://localhost:8080'
+	};
+	return gulp.src('./index.html')
+		.pipe(gOpen('', options));
+});
 
-	gulp.task('default', function() {
-		runSequence('clear-dev', 'compile-javascript',
-			'compile-css', 'uncache-index', 'copy-fonts',
-			function() {
-				gulp.run('start-server');
-				gulp.run('watch-changes');
-				gulp.run('open-browser');
-			});
-	});
+gulp.task('default', function() {
+	runSequence('clear-dev', 'compile-javascript',
+		'compile-css', 'uncache-index', 'copy-fonts',
+		function() {
+			gulp.run('start-server');
+			gulp.run('watch-changes');
+			gulp.run('open-browser');
+		});
+});
 }());
